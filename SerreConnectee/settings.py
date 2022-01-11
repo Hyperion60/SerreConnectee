19eaa8b3abfb1c67215e9d9029c7794db1f20c77
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-01-11 18:12:29 +0100

    User: Remove dedicated apps

--- SerreConnectee/settings.py
+++ SerreConnectee/settings.py
@@ -90,8 +90,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = "SerreUser.SerreUser"
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- SerreConnectee/views.py
+++ SerreConnectee/views.py
@@ -1,4 +1,20 @@
+from django.shortcuts import render
 
 
 def about(request):
     pass
+
+
+def login(request):
+    context = {
+        'next': request.GET.get('next', ''),
+        'errors': [],
+    }
+    if request.user.is_authenticated:
+        context['errors'].append("Vous êtes déjà connecté !")
+        return render(request, "index.html", context)
+
+    if request.POST:
+        pass
+
+    return render(request, "User/login.html", context)
