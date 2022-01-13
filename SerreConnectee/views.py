2af40185d701856b30bec3d5e4ec5c4316354347
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-01-13 11:33:54 +0100

    login: Add login function

--- SerreConnectee/views.py
+++ SerreConnectee/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 def about(request):
@@ -9,16 +13,32 @@ def index(request):
     return render(request, "index.html")
 
 
-def login(request):
+def login_user(request):
     context = {
-        'next': request.GET.get('next', ''),
+        'next': request.GET.get('next', '/'),
         'errors': [],
+        'restricted': True,
     }
     if request.user.is_authenticated:
         context['errors'].append("Vous êtes déjà connecté !")
         return render(request, "index.html", context)
 
     if request.POST:
-        pass
+        user = None
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+            User.objects.get(username__exact=username)
+            user = authenticate(username=username, password=password)
+            if not user.check_password(password):
+                context['errors'].append("Mot de passe invalide")
+        except MultiValueDictKeyError:
+            context['errors'].append("Champ(s) manquant(s)")
+        except User.DoesNotExist:
+            context['errors'].append("Utilisateur introuvable")
+
+        if not len(context['errors']) and user:
+            login(request, user)
+            return redirect(context['next'])
 
     return render(request, "User/login.html", context)
