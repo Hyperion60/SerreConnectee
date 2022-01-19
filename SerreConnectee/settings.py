012f2ce6149cbaef8a3f93e992299364b67632c6
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-01-19 10:50:42 +0100

    env: Load dotenv for production

--- SerreConnectee/settings.py
+++ SerreConnectee/settings.py
@@ -17,7 +17,8 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment file into development environment
-load_dotenv(BASE_DIR / "deploiements/secret.env")
+if not os.getenv("PRODUCTION"):
+    load_dotenv(BASE_DIR / "deploiements/secret.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
