ba66dcc03141aa086fa28215a6fee93a3b9c8a15
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-02-14 09:16:29 +0100

    Serre: Add conversion utc to cet

--- Serre/apps.py
+++ Serre/apps.py
@@ -1,3 +1,6 @@
+import datetime
+
+import pytz
 from django.apps import AppConfig
 
 
@@ -8,10 +11,17 @@ class SerreConfig(AppConfig):
     # Création du compte super-utilisateur
     def ready(self):
         from django.contrib.auth.models import User
+        from Serre.models import Releves
+        from SerreConnectee.settings import TIME_ZONE
         import sqlite3
 
         try:
             User.objects.get(username__exact="admin")
+            releves = Releves.objects.all()
+            for releve in releves:
+                if releve.timestamp.tzinfo == datetime.timezone.utc:
+                    releve.replace(hour=releve.hour + 1, tzinfo=pytz.timezone(TIME_ZONE))
+                    releve.save()
         except User.DoesNotExist:
             admin = User(username="admin", is_superuser=True, is_staff=True, is_active=True)
             admin.set_password("Admin80080")
