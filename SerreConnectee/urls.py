e4805e28b276c2b28e6a5b180a651dc2f09fa046
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2022-02-14 09:07:07 +0100

    Serre: Remove useless url path

--- SerreConnectee/urls.py
+++ SerreConnectee/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 from SerreConnectee import views, account
-import Serre
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -34,9 +33,7 @@ urlpatterns = [
     path('recover/', views.recover_password, name="recover-password"),
     path('serre/', include('Serre.urls')),
     path('signup/', views.signup_user, name="signup"),
-    path('test/', views.test_arduino),
     path('delete/<int:pk>/', views.user_ask_delete, name="ask-delete"),
     re_path(r'^delete/(?P<uidb64>[0-9A-Za-z_\-\']+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$',
             views.user_delete, name="delete-user"),
-    # path('serre/get/token/', Serre.views)
 ]
